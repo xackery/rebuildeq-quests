bab115690435b58276bf246febe26853c3381c42
RebuildEQ <RebuildEQ@gmail.com>
2023-06-29 07:10:22 +0000

    Add guild_master and bounty_quest

---
+++ lua_modules/bounty_quest.lua
@@ -0,0 +1,18 @@
+local bountyquest = {}
+
+---@param e NPCEventSay
+function bountyquest.do_dialog(e)
+    local mob_class = e.self:GetClass();
+
+    if (mob_class == 28) then rogue_dialog(e)
+    end
+end
+
+---@param e NPCEventSay
+function rogue_dialog(e)
+    if (e.message:findi("guild business")) then
+        e.self:Say("Hello, adventurer. If you are interested in becoming a member of the guild, you must first [" .. eq.say_link("how do i prove myself?",false,"prove yourself") .. "].")
+    end
+end
+
+return bountyquest;
